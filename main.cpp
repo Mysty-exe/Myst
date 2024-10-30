a0dde19ca0c3b73df2713618d96b4aea57a4eb45
Mysty-exe <o.s.toure72@gmail.com>
2024-10-30 11:55:17 -0400

    fixed bugs with backspace and return

--- main.cpp
+++ main.cpp
@@ -5,6 +5,23 @@
 
 using namespace std;
 
+void rewriteScreen(vector<string> lines, int x, int y, bool highlight = false)
+{
+    int tempX, tempY;
+    tempX = tempY = 0;
+
+    clear();
+    move(tempY, tempX);
+
+    for (const string &line : lines)
+    {
+        mvprintw(tempY, tempX, line.c_str());
+        tempY++;
+    }
+
+    move(y, x);
+}
+
 main(int argc, char **argv)
 {
     int width, height = 0;
@@ -12,13 +29,14 @@ main(int argc, char **argv)
     vector<string> lines = {""};
 
     initscr();
-    keypad(stdscr, true);
     noecho();
+    raw();
+    keypad(stdscr, true);
+    getmaxyx(stdscr, height, width);
 
-    while (true)
+    int c;
+    while ((c = getch()) != 27)
     {
-        int c = getch();
-
         if (c >= 32 && c <= 126)
         {
             if (cursorX == lines.at(cursorY).length())
@@ -36,12 +54,28 @@ main(int argc, char **argv)
 
         switch (c)
         {
+        case 1:
+            rewriteScreen(lines, cursorX, cursorY, true);
+            break;
         case 8:
-            move(cursorY, cursorX - 1);
-            delch();
-            if (cursorX == 0)
+            if (cursorX > 0)
             {
-                move(cursorY - 1, cursorX);
+                lines.at(cursorY) = lines.at(cursorY).erase(cursorX - 1, 1);
+                move(cursorY, cursorX - 1);
+                delch();
+            }
+            if (cursorY > 0)
+            {
+                if (cursorX == 0)
+                {
+                    int x = lines.at(cursorY - 1).length();
+                    deleteln();
+                    move(cursorY - 1, x);
+                    lines.at(cursorY - 1) = lines.at(cursorY - 1) + lines.at(cursorY);
+                    printw(lines.at(cursorY).c_str());
+                    lines.erase(lines.begin() + cursorY);
+                    move(cursorY - 1, x);
+                }
             }
             break;
         case 10:
@@ -51,7 +85,7 @@ main(int argc, char **argv)
                 insertln();
                 move(cursorY + 1, 0);
             }
-            else if (cursorX == lines.at(cursorY).length())
+            else if (cursorX == lines.at(cursorY).length() - 1)
             {
                 move(cursorY + 1, 0);
                 lines.insert(lines.begin() + cursorY + 1, "");
@@ -62,6 +96,7 @@ main(int argc, char **argv)
                 string text = lines.at(cursorY).substr(cursorX, lines.at(cursorY).length() - 1);
                 clrtoeol();
                 move(cursorY + 1, 0);
+                lines.at(cursorY) = lines.at(cursorY).substr(0, cursorX);
                 lines.insert(lines.begin() + cursorY + 1, text);
                 insertln();
                 printw(text.c_str());
